6a3e0db1c44dfceccc40c6a4029b0038f29d9a28
vincent-tsugranes <vtsugranes@gmail.com>
2020-08-27 09:34:57 -0400

    Added core project stub

---
+++ dotnet.cafe.core/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using dotnet.cafe.domain;
+
+namespace dotnet.cafe.core
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            LineItem item = new LineItem(item: Item.COFFEE_BLACK,"Vince");
+            Console.WriteLine("Initialized: " + item.ToString());
+            
+            var counter = 0;
+            var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+            while (max == -1 || counter < max)
+            {
+                Console.WriteLine($"Counter: {++counter}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
